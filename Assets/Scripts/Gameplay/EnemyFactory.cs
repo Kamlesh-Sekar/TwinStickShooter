f6113c29d853e345830ae53f6d7ba8e0df1b82bb
kamlesh_gamesmith <kamlesh@gamesmith.in>
2025-10-24 23:08:23 +0530

    - Implemented zombie and player mesh - Implemented animations

--- Assets/Scripts/Gameplay/EnemyFactory.cs
+++ Assets/Scripts/Gameplay/EnemyFactory.cs
@@ -3,10 +3,11 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Factories/EnemyFactory")]
 public class EnemyFactory : ScriptableObject
 {
-    [SerializeField] private Enemy enemy;
+    [SerializeField] private Enemy[] enemies;
 
     public Enemy Create(Transform positionTransform)
     {
+        Enemy enemy = enemies[Random.Range(0, enemies.Length)];
         return Instantiate(enemy, positionTransform.position, positionTransform.rotation);
     }
 }
